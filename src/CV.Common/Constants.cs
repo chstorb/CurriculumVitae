20645a25a84f40ee127991c02d59b3db57d2b86f
chstorb_cp <SND\chstorb_cp>
2014-04-18 20:52:36 +0000

    initial checkin

---
+++ src/CV.Common/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV.Common
+{
+    /// <summary>
+    /// The Constants class contains library wide constants.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The table prefix for all tables
+        /// </summary>
+        public const string TablePrefix = "Cv";
+    }
+}
