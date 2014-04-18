112fab768e9703142c2018f0b9964452366f2536
chstorb_cp <SND\chstorb_cp>
2014-04-18 21:19:57 +0000

    initial checkin

---
+++ src/CV.Diagnostics/CustomDbCommandInterceptor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV.Diagnostics
+{
+    using System.Data.Common;  
+    using System.Data.Entity.Infrastructure.Interception;
+    using System.Diagnostics;
+    using CV.Diagnostics.Logging;
+    
+    /// <summary>
+    /// The CustomDbCommandIterceptor class will log every SQL query that is sent to the database
+    /// For successful queries or commands, this code writes an Information log with latency 
+    /// information. For exceptions, it creates an Error log. 
+    /// </summary>
+    public class CustomDbCommandInterceptor : DbCommandInterceptor
+    {
+        private ILogger _logger = new Logger();
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        public override void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            base.ScalarExecuting(command, interceptionContext);
+            _stopwatch.Restart();
+        }
+
+        public override void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            _stopwatch.Stop();
+            if (interceptionContext.Exception != null)
+            {
+                _logger.Error(interceptionContext.Exception, "Error executing command: {0}", command.CommandText);
+            }
+            else
+            {
+                _logger.TraceApi("SQL Database", "CustomDbCommandInterceptor.ScalarExecuted", _stopwatch.Elapsed, "Command: {0}: ", command.CommandText);
+            }
+            base.ScalarExecuted(command, interceptionContext);
+        }
+
+        public override void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            base.NonQueryExecuting(command, interceptionContext);
+            _stopwatch.Restart();
+        }
+
+        public override void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            _stopwatch.Stop();
+            if (interceptionContext.Exception != null)
+            {
+                _logger.Error(interceptionContext.Exception, "Error executing command: {0}", command.CommandText);
+            }
+            else
+            {
+                _logger.TraceApi("SQL Database", "CustomDbCommandInterceptor.NonQueryExecuted", _stopwatch.Elapsed, "Command: {0}: ", command.CommandText);
+            }
+            base.NonQueryExecuted(command, interceptionContext);
+        }
+
+        public override void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            base.ReaderExecuting(command, interceptionContext);
+            _stopwatch.Restart();
+        }
+        public override void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            _stopwatch.Stop();
+            if (interceptionContext.Exception != null)
+            {
+                _logger.Error(interceptionContext.Exception, "Error executing command: {0}", command.CommandText);
+            }
+            else
+            {
+                _logger.TraceApi("SQL Database", "CustomDbCommandInterceptor.ReaderExecuted", _stopwatch.Elapsed, "Command: {0}: ", command.CommandText);
+            }
+            base.ReaderExecuted(command, interceptionContext);
+        }
+    }
+}
---
+++ src/CV.Diagnostics/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CV.Diagnostics")]
+[assembly: AssemblyDescription("Curriculum Vitae Diagnostics")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("STC STORB Consulting")]
+[assembly: AssemblyProduct("Curriculum Vitae")]
+[assembly: AssemblyCopyright("Copyright ©  STC STORB Consulting 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("627230ec-05df-46b8-8316-b3d41db333dc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
