6a51ef3d82d3ba6383847aa3469e396f9ef3e048
chstorb_cp <SND\chstorb_cp>
2014-04-21 09:58:56 +0000

    initial checkin

---
+++ src/CV.DAL/CustomDbConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+
+namespace CV.DAL
+{
+    using System.Data.Entity.Infrastructure.Interception;
+    using CV.Diagnostics;
+
+    /// <summary>
+    /// CustomDbConfiguration class.
+    /// </summary>
+    public class CustomDbConfiguration : DbConfiguration
+    {
+        public CustomDbConfiguration()
+        {
+            SetHistoryContext("System.Data.SqlClient", (connection, defaultSchema) => new CustomHistoryContext(connection, defaultSchema));
+
+            // Enable connection resiliency
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+#if DEBUG
+            DbInterception.Add(new CustomDbCommandInterceptor());
+            DbInterception.Add(new TransientErrorDbCommandInterceptor());
+#endif
+        }
+    }
+}
---
+++ src/CV.DAL/CustomHistoryContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Migrations.History;
+using System.Linq;
+using System.Web;
+
+namespace CV.DAL
+{
+    using CV.Common;
+
+    /// <summary>
+    /// CustomHistoryContext class.
+    /// </summary>
+    public class CustomHistoryContext : HistoryContext
+    {
+        public CustomHistoryContext(DbConnection dbConnection, string defaultSchema)
+            : base(dbConnection, defaultSchema)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<HistoryRow>().ToTable(tableName: Constants.TablePrefix + "MigrationHistory");
+        }
+    } 
+}
